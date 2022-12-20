0a856d57ec6cb841aa95199f58410621f021fe24
Patrick Cypriano <patrickcyprianop@gmail.com>
2022-12-20 09:24:39 -0300

    alterando cadastro.js de pages exercicios

--- src/pages/exercicio/Cadastro.js
+++ src/pages/exercicio/Cadastro.js
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import axios from "axios";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
-import FormAluno from "../../components/aluno/FormAluno";
+import FormTipoExercicio from "../../components/exercicio/FormTipoExercicio"; 
 import InformModal from "../../components/utils/InformModal";
 
 const Cadastro = () => {
@@ -17,17 +17,9 @@ const Cadastro = () => {
     //https://github.com/jquense/yup
     const validator = yup.object().shape({
         nome: yup.string().required("Nome é obrigatório."),
-        dataNascimento: yup.date().required("Data de nascimento é obrigatória."),
-        cpf: yup.string().length(11, "CPF está incompleto.").required("CPF é obrigatório."),
-        sexo: yup.string().oneOf(["M", "F", "O"], "Gênero está incorreto.").required("Gênero é obrigatório."),
-        telefone: yup.string().length(11, "Telefone está incompleto.").required("Telefone é obrigatório."),
-        email: yup.string().email("E-mail inválido.").required("E-mail é obrigatório."),
-        senha: yup.string().min(6, "Senha deve ter pelo menos 6 caracteres.").max(12, "Senha deve ter no máximo 12 caracteres.").required("Senha é obrigatória."),
-        confSenha: yup
-            .string()
-            .oneOf([yup.ref("senha"), null], "Confirmação de Senha e Senha devem ser iguais.")
-            .required("Confirmação de Senha é obrigatória."),
-        ativo: yup.boolean().required("Situação é obrigatória."),
+        pesoMinimo: yup.number().required("Peso minimo é obrigatorio"),
+        pesoMaximo: yup.number().required("Peso maximo é obrigatorio"),
+        degrauPeso: yup.number().required("Degrau peso é obrigatorio")
     });
 
     function handleChange(event) {
@@ -94,11 +86,11 @@ const Cadastro = () => {
     return (
         <>
             <div className="d-flex justify-content-between align-items-center">
-                <h1>Novo Aluno</h1>
+                <h1>Novo Tipo de Exercício</h1>
             </div>
             <hr />
             <form onSubmit={handleSubmit} noValidate autoComplete="off">
-                <FormAluno handleChange={handleChange} inputs={inputs} errors={errors} isNew={true} />
+                <FormTipoExercicio handleChange={handleChange} inputs={inputs} errors={errors} isNew={true} />
                 <div className="mt-3">
                     <Link to="/tipoexercicio" className="btn btn-secondary me-1">
                         Cancelar
@@ -108,7 +100,7 @@ const Cadastro = () => {
                     </button>
                 </div>
             </form>
-            <InformModal info="Aluno cadastrado com sucesso!" action={closeModalAndRedirect} />
+            <InformModal info="Tipo de exercicio cadastrado com sucesso!" action={closeModalAndRedirect} />
         </>
     );
 };
